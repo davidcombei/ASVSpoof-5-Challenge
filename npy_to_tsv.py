1aead0eda5051c4e864df61bff87661e9735bbd6
davidcombei <102519601+davidcombei@users.noreply.github.com>
2024-07-24 21:29:50 +0300

    VM

--- npy_to_tsv.py
+++ npy_to_tsv.py
@@ -1,19 +1,13 @@
 import numpy as np
 
-# Load the npy file
 scores = np.load('/home/asvspoof/work/ASVSpoof2024/CNN/model_scores.npy')
 
-# Assuming you have a list of filenames corresponding to the scores
-# You need to replace this with the actual code to get filenames
-filenames = [f"E_{i:010}.png" for i in range(len(scores))]  # Example filenames
+filenames = [f"E_{i:010}.png" for i in range(len(scores))]  
 
-# Ensure filenames and scores have the same length
 assert len(filenames) == len(scores), "Mismatch between number of filenames and scores"
 
-# Combine filenames and scores
 results = list(zip(filenames, scores))
 
-# Save to a TSV file
 with open('/home/asvspoof/work/ASVSpoof2024/CNN/model_scores.tsv', 'w') as f:
     for filename, score in results:
         f.write(f"{filename}\t{score}\n")
